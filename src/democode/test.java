80af13eac257da569c4851ecb261fa27b24afd82
ramsannidhi <think.mad96@gmail.com>
2020-07-08 23:59:52 +0530

    first commit.....

---
+++ src/democode/test.java
@@ -0,0 +1,16 @@
+package democode;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry = "yes";
+		if(hungry == "yes") {
+			System.out.println("eat something");
+		}else {
+			System.out.println("do work ...");
+		}
+
+	}
+
+}
